e80e0b2405efd213ae2c2f97c07e5d28e810148e
Callum <callum.git@outlook.com>
2020-05-27 12:35:59 +0100

    Removed API key

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,8 +7,9 @@ class Content extends Component {
     componentDidMount() {
         let lat = '55.95';
         let lon = '-3.15';
+        const secret = process.env.REACT_APP_SECRET
 
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`).then(res => {
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${secret}&units=metric`).then(res => {
             console.log(res)
             this.setState({ location: res.data.name, temp: res.data.main.temp.toFixed(1), feelsLike: res.data.main.feels_like.toFixed(1), type: res.data.weather[0].main, icon: res.data.weather[0].icon, windSpeed: res.data.wind.speed, countryCode: res.data.sys.country, typeClassName: res.data.weather[0].main.toLowerCase() })
         })
