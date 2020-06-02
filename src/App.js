ac9a2dc0633564b454cf9e8838e75a4394e03eee
Callum <callum.git@outlook.com>
2020-06-02 17:37:10 +0100

    Added search, basic error handling, random backgrounds

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import './styles.min.css'
 
 import Content from './components/Content';
-// import Search from './components/Search';
 
+const secret = process.env.REACT_APP_SECRET;
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Search></Search> */}
-      <Content></Content>
-    </div>
-  );
+class App extends Component {
+  state = { errorMsg: '', showClass: '', hideClass: 'hide', query: null, location: '', countryCode: '', temp: '', feelsLike: '', type: '', windSpeed: '', icon: '', typeClassName: '' };
+
+  getData = () => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&appid=${secret}&units=metric`).then(res => {
+    console.log(res);
+    this.setState({ hideClass: null, location: res.data.name, temp: res.data.main.temp.toFixed(1), feelsLike: res.data.main.feels_like.toFixed(1), type: res.data.weather[0].main, icon: res.data.weather[0].icon, windSpeed: res.data.wind.speed, countryCode: res.data.sys.country, typeClassName: res.data.weather[0].main.toLowerCase() });
+  })
+    .catch((error) => {
+      if (error.response) {
+        this.showError();
+      }
+    });
+
+  showError = (error) => {
+    this.setState({ showClass: 'show' });
+    this.setState({ errorMsg: 'Location not found' })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.query !== null) {
+      this.getData();
+      this.setState({ showClass: null });
+    } else {
+      this.setState({ showClass: "show" });
+      this.setState({ errorMsg: 'Please enter a location' })
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ query: e.target.value })
+  }
+
+  componentDidUpdate() {
+    document.title = `theweather.xyz | ${this.state.location}`
+  };
+
+  render() {
+
+    const { errorMsg, showClass, typeClassName, hideClass } = this.state;
+    return (
+      <div className="App">
+        <div className={`container default-bg ${typeClassName}`}>
+          <form className={`${showClass}`} onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Enter a location" ref={input => this.search = input} onChange={this.handleInputChange}></input>
+            <div id="warning" className={`warning hide ${showClass}`}>{errorMsg}</div>
+            <button className="button">Search</button>
+          </form>
+          <div className={`${hideClass}`}>
+            <Content weather={this.state}></Content>
+          </div>
+          <div className="credits"><a href="https://github.com/ScotDev" rel="noopener noreferrer" target="_blank"
+          >Created by ScotDev <i className="fab fa-github"></i></a></div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,42 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class Content extends Component {
-    state = { location: '', countryCode: '', temp: '', feelsLike: '', type: '', windSpeed: '', icon: '', typeClassName: '' }
 
-    componentDidMount() {
 
-        const secret = process.env.REACT_APP_SECRET
-
-        let searchLocation = 'Edinburgh'
-
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=${secret}&units=metric`).then(res => {
-            console.log(res);
-            this.setState({ location: res.data.name, temp: res.data.main.temp.toFixed(1), feelsLike: res.data.main.feels_like.toFixed(1), type: res.data.weather[0].main, icon: res.data.weather[0].icon, windSpeed: res.data.wind.speed, countryCode: res.data.sys.country, typeClassName: res.data.weather[0].main.toLowerCase() });
-        })
-            .catch(error => {
-                console.log(error)
-            });
-
-    };
-
-    componentDidUpdate() {
-        document.getElementById('favicon').href = `https://openweathermap.org/img/wn/${this.state.icon}@2x.png`
-        document.title = `theweather.xyz | ${this.state.location}`
-    };
 
     render() {
-        const { icon, location, countryCode, temp, feelsLike, type, windSpeed, typeClassName } = this.state;
+        const { icon, location, countryCode, temp, feelsLike, type, windSpeed } = this.props.weather;
 
         return (
-            <div id="bg" className={`container ${typeClassName}`}>
-                <span><h1>{location}, {countryCode} <i className="fas fa-map-marker-alt"></i></h1></span>
-                <span><h2>Current temperature: {temp}°C</h2></span>
-                <span><h3>Feels like: {feelsLike}°C</h3></span>
-                <span><h4>{type}</h4><img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt=''></img></span>
-                <span><h5>Wind: {windSpeed} mph</h5></span>
-                <div className="credits"><a href="https://github.com/ScotDev" rel="noopener noreferrer" target="_blank"
-                >Created by ScotDev <i className="fab fa-github"></i></a></div>
+            <div className="helper-bg">
+                <h1>{location}, {countryCode} <i className="fas fa-map-marker-alt"></i></h1>
+                <h2>Current: {temp}°C</h2>
+                <h3>Feels like: {feelsLike}°C</h3>
+                <h4>{type}</h4><img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt=''></img>
+                <h5>Wind: {windSpeed} mph</h5>
             </div>
         )
 
