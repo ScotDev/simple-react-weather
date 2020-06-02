909269c738e723f1d2eea67dfb39c6c9fbd6bf5d
Callum <callum.git@outlook.com>
2020-06-02 22:23:41 +0100

    Fixed form width

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,7 +11,7 @@ class Content extends Component {
             <div className="helper-bg">
                 <h1>Current: {temp}°C</h1>
                 <h2>Feels like: {feelsLike}°C</h2>
-                <h3>{type}</h3><img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt=''></img>
+                <h3>{type}</h3><img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt=''></img>
                 <h4>Wind: {windSpeed} mph</h4>
                 <h5>{location}, {countryCode} <i className="fas fa-map-marker-alt"></i></h5>
             </div>
