74b1edebeb5c3823f9cad2265bc211f5e062df83
Callum <callum.git@outlook.com>
2020-06-05 15:23:26 +0100

    Changed to local imgs to improve loading time. Added bg randomiser

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 import './styles.min.css'
 
 import Content from './components/Content';
-const secret = process.env.REACT_APP_SECRET;
+const secret = '2a6ae01e17df3c070d45fcd262b3d837';
+// const secret = process.env.REACT_APP_SECRET;
 
 class App extends Component {
-  state = { errorMsg: '', showClass: '', hideClass: 'hide', query: null, location: '', countryCode: '', temp: '', feelsLike: '', type: '', windSpeed: '', icon: '', typeClassName: '' };
+  state = { randomNumberTo2: '', randomNumberTo7: '', errorMsg: '', showClass: '', hideClass: 'hide', query: null, location: '', countryCode: '', temp: '', feelsLike: '', type: '', windSpeed: '', icon: '', typeClassName: '' };
 
   getData = () => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&appid=${secret}&units=metric`).then(res => {
     this.setState({ hideClass: null, location: res.data.name, temp: res.data.main.temp.toFixed(1), feelsLike: res.data.main.feels_like.toFixed(1), type: res.data.weather[0].main, icon: res.data.weather[0].icon, windSpeed: res.data.wind.speed.toFixed(1), countryCode: res.data.sys.country, typeClassName: res.data.weather[0].main.toLowerCase() });
@@ -21,7 +22,7 @@ class App extends Component {
   showError = () => {
     this.setState({ showClass: 'show' });
     this.setState({ errorMsg: 'Location not found' })
-  }
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -32,22 +33,32 @@ class App extends Component {
       this.setState({ showClass: "show" });
       this.setState({ errorMsg: 'Please enter a location' })
     }
-  }
+  };
 
   handleInputChange = (e) => {
     this.setState({ query: e.target.value })
-  }
+  };
 
+  bgRandomiser = () => {
+    const randomNumberTo7 = Math.floor(Math.random() * 7);
+    const randomNumberTo2 = Math.floor(Math.random() * 2);
+    this.setState({ randomNumberTo7: randomNumberTo7 })
+    this.setState({ randomNumberTo2: randomNumberTo2 })
+  };
+
+  componentDidMount() {
+    this.bgRandomiser()
+  }
   componentDidUpdate() {
     document.title = `theweather.xyz | ${this.state.location}`
   };
 
   render() {
 
-    const { errorMsg, showClass, typeClassName, hideClass } = this.state;
+    const { randomNumberTo7, randomNumberTo2, errorMsg, showClass, typeClassName, hideClass } = this.state;
     return (
       <div className="App">
-        <div className={`container default-bg ${typeClassName}`}>
+        <div className={`container bg default-bg-${randomNumberTo7} weather-bg-${typeClassName}-${randomNumberTo2}`}>
           <form className={`${showClass}`} onSubmit={this.handleSubmit}>
             <input type="text" placeholder="Enter a location" ref={input => this.search = input} onChange={this.handleInputChange}></input>
             <div id="warning" className={`warning hide ${showClass}`}>{errorMsg}</div>
