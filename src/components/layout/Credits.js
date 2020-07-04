e3e63ed06dc7c2128769ae76de714c71c3fdfefe
Callum <callum.git@outlook.com>
2020-07-04 19:29:56 +0100

    Tweaked credits

--- src/components/layout/Credits.js
+++ src/components/layout/Credits.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function Credits({ photoUrl }) {
     return (
         <div className="credits"><a href="https://github.com/ScotDev" rel="noopener noreferrer" target="_blank"
-        >Created by ScotDev <i className="ri-github-fill"></i></a><a href={photoUrl} rel="noopener noreferrer" target="_blank"> Photos provided by Pexels</a></div>
+        >Created by ScotDev <i className="ri-github-fill"></i></a><a href={photoUrl} rel="noopener noreferrer" target="_blank"> - Photos provided by Pexels</a></div>
     )
 }
