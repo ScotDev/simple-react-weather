913a3b2848e99003000d8b21135ad96d8b1f2745
Callum <callum.git@outlook.com>
2020-12-05 14:40:47 +0000

    Fixed dropdown width issue, added error translations

--- src/components/reducers/rootReducer.js
+++ src/components/reducers/rootReducer.js
@@ -7,9 +7,6 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
     if (action.type === "CHANGE_USER_LANG") {
-        console.log("State is: ", state)
-        console.log("Action is: ", action)
-
         let copyOfLangOptions = state.langOptions.filter((item) => item !== action.payload)
         copyOfLangOptions.unshift(action.payload);
         return {
