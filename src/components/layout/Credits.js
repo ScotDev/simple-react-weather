4e5a719538062ee548a3ca0e5a80762f1af75f54
Callum <callum.git@outlook.com>
2021-03-07 09:08:39 +0000

    .

--- src/components/layout/Credits.js
+++ src/components/layout/Credits.js
@@ -6,13 +6,13 @@ function Credits(props) {
         <div className="credits"><a href="https://github.com/ScotDev" rel="noopener noreferrer" target="_blank"
         >{props.langVals.credits.created} ScotDev <i className="ri-github-fill"></i></a><a href='https://www.pexels.com/' rel="noopener noreferrer" target="_blank"> - {props.langVals.credits.pexels} Pexels</a></div>
     )
-}
+};
 
 const mapStateToProps = state => {
     return {
         langOptions: state.langOptions,
         langVals: state.langVals
     }
-}
+};
 
 export default connect(mapStateToProps)(Credits);
